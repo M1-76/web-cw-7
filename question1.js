63fd6c38e006422bd82d96fbf267208fcda399cd
Mohammed Elsamad M1-76 <mohammed.elsamadss@gmail.com>
2021-01-05 21:43:14 +0300

    done

--- question1.js
+++ question1.js
@@ -1,1 +1,16 @@
 // قم بحل السؤال الأول هنا
+let pets = ("dog", "cat", "horse", "rabbit", "bird")
+
+pets.pop();
+pets.push(`bear`)
+console.log(pets)
+
+let organim = {
+    name: `Camel`,
+    age: `3 year`,
+    type: `Mammal`,
+    emoji: `🐪`
+
+}
+
+console.log(organim.age)
